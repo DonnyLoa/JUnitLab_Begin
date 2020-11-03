5db8e51cdf1ec813442ebc7989c6979a1fb92f9e
Carter Bryant <donny.lao64@gmail.com>
2020-11-03 15:05:21 -0800

    Added deduct() method and added call to assertEquals in getIssuingStore
    call to assertEquals() in getIssuingStore() test is used to test
    getBalance() method

---
+++ src/main/java/csc131/junit/GiftCardTest.java
@@ -0,0 +1,38 @@
+package csc131.junit;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+
+public class GiftCardTest
+{
+	@Test
+	public void getIssuingStore()
+	{
+		double balance;
+		GiftCard card;
+		int issuingStore;
+
+		issuingStore = 1337;
+		balance = 100.00;
+		card = new GiftCard(issuingStore, balance);
+
+		assertEquals("getBalance()", balance, card.getBalance(), 0.001);
+		assertEquals("getIssuingStore()", issuingStore, card.getIssuingStore());
+	}
+	
+	/*@Test
+	public void getBalance() 
+	{
+		GiftCard card = new GiftCard(1337,100.00);
+		assertEquals("getBalance()", 100.00, card.getBalance(), 0.001);
+	}*/
+	
+	@Test
+	public String deduct(double amount) 
+	{
+		GiftCard card = new GiftCard(1337,100.00);
+		return card.deduct(amount);
+	}
+}
