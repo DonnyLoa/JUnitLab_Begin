df3494ef71b1e0c39e5b730640d3b9cf2ac2c190
Carter Bryant <donny.lao64@gmail.com>
2020-11-04 16:38:04 -0800

    Uncommented the getBalance() test and erased 1st call to assertEquals

--- src/main/java/csc131/junit/GiftCardTest.java
+++ src/main/java/csc131/junit/GiftCardTest.java
@@ -18,16 +18,15 @@ public class GiftCardTest
 		balance = 100.00;
 		card = new GiftCard(issuingStore, balance);
 
-		assertEquals("getBalance()", balance, card.getBalance(), 0.001);
 		assertEquals("getIssuingStore()", issuingStore, card.getIssuingStore());
 	}
 	
-	/*@Test
+	@Test
 	public void getBalance() 
 	{
 		GiftCard card = new GiftCard(1337,100.00);
 		assertEquals("getBalance()", 100.00, card.getBalance(), 0.001);
-	}*/
+	}
 	
 	@Test
 	public void deduct() 
